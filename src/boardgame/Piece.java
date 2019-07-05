add271c719d25b2b0cf74866b58f8d51c090b6a2
Lucas Bueno <lucas-50p@hotmail.com>
2019-07-05 18:14:03 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;
 	private Board board;
@@ -17,5 +17,21 @@ public class Piece {
 	//public void setBoard(Board board) {
 	//	this.board = board;// APAGAR PARA NO SER ALTERADO
 	
-	
+	public abstract boolean[][] possibleMoves();
+		
+	public boolean possibleMove(Position position){
+		return possibleMoves()[position.getRow()][position.getColumn()];//UM METODO CONCRETO,() QUE ESTÁ USANDO UM METODO ABSTRATO. METODO QUE FAZ UM GANÇHO COM A SUB CLASSE 
+	}
+		
+	public boolean isThereAnyPossibleMove(){
+		boolean[][] mat = possibleMoves();
+		for(int i=0; i<mat.length; i++){
+			for(int j=0; j<mat.length; j++){//MATRIZ DO XADREZ É QUADRADA
+			if (mat[i][j]){
+				return true;
+				}
+			}
+		}
+	return false;
+	}
 }
